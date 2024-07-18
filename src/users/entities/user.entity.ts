095696ec3e329832b2e57a5aeeebb3157facdbd3
GOH23 <padadamlpd@gmail.com>
2024-07-18 22:57:01 +0300

    update user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,26 @@
 import { Link } from "src/links/entities/link.entity";
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-
+enum UserRoles {
+    Admin = "Admin",
+    User = "User",
+    Moderator = "Moderator",
+    Premium  = "Premium",
+}
 @Entity()
 export class User {
     @PrimaryGeneratedColumn("uuid")
     Uuid: string;
-    @Column()
+    @Column({default: false})
     IsPremium: boolean;
+     
     @Column()
-    ProfileImage: string;
+    Email: string;
+    @Column()
+    Password: string;
+    @Column()
+    Username: string;
+    @Column({type: 'enum',enum: UserRoles,default: UserRoles.User})
+    Role: UserRoles
     @OneToOne(()=> Link)
     @JoinColumn()
     Links: Link[]
